5660e3a9893d544ae9d889ebe076bf34162b9a15
Terab Norbu Tsering <terab.dev@gmail.com>
2022-12-07 16:54:42 +1100

    Added new program post type

--- archive-event.php
+++ archive-event.php
@@ -17,8 +17,11 @@ get_header(); ?>
 <?php while( have_posts()) : the_post(); ?>
     <div class="event-summary">
         <a class="event-summary__date t-center" href="<?php site_url(); ?>">
-        <span class="event-summary__month">Apr</span>
-        <span class="event-summary__day">02</span>  
+            <span class="event-summary__month"><?php 
+                $eventDate = new DateTime(get_field('event_date'));
+                print $eventDate->format('M');
+            ?></span>
+            <span class="event-summary__day"><?php print $eventDate->format('d'); ?></span>  
         </a>
         <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
@@ -28,6 +31,10 @@ get_header(); ?>
 
 <?php endwhile; ?>
 <?php print paginate_links(); ?>
+
+<hr class="section-break">
+
+<p>Looking for an recap of past events? <a href="<?php print site_url('/past-events'); ?>">Check out our past events archive </a></p>
 </div>
 
 <?php get_footer();
