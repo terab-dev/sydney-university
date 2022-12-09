17275019ff4fcf93f200bffbc176978bc3cab8b2
Terab Norbu Tsering <terab.dev@gmail.com>
2022-12-09 21:06:41 +1100

    Added single pages

--- single-professor.php
+++ single-professor.php
@@ -2,21 +2,10 @@
   
   get_header();
 
-  while( have_posts() ) : the_post(); ?>
+  while( have_posts() ) : the_post(); 
   
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php 
-        $pageBannerImage = get_field('page_banner_background_image'); 
-        print $pageBannerImage['sizes']['pageBanner'];
-      ?>);"></div>
-
-<div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title"><?php the_title(); ?></h1>
-    <div class="page-banner__intro">
-        <p><?php the_field('page_banner_subtitle'); ?></p>
-    </div>
-</div>  
-</div>
+  pageBanner();
+  ?>
     <div class="container container--narrow page-section">
       
 
