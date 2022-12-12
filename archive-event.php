3d9de7ab1d2e3b46c21acfad3749c40b18fc10aa
Terab Norbu Tsering <terab.dev@gmail.com>
2022-12-12 20:50:30 +1100

    Archive pages added

--- archive-event.php
+++ archive-event.php
@@ -1,35 +1,22 @@
 <?php 
 
-get_header(); ?>
+get_header(); 
 
-<div class="page-banner">
-<div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-<div class="page-banner__content container container--narrow">
-  <h1 class="page-banner__title">All Events</h1>
-  <div class="page-banner__intro">
-    <p>See whats going here.</p>
-  </div>
-</div>  
-</div>
+pageBanner([
+    'title' =>'All Events',
+    'subtitle' => 'See what is going on in our world!'
+]);
+
+?>
+
 
 <div class="container container--narrow page-section">
 
-<?php while( have_posts()) : the_post(); ?>
-    <div class="event-summary">
-        <a class="event-summary__date t-center" href="<?php site_url(); ?>">
-            <span class="event-summary__month"><?php 
-                $eventDate = new DateTime(get_field('event_date'));
-                print $eventDate->format('M');
-            ?></span>
-            <span class="event-summary__day"><?php print $eventDate->format('d'); ?></span>  
-        </a>
-        <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-            <p><?php print wp_trim_words(get_the_content(), 18) ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-        </div>
-    </div>
+<?php while( have_posts()) : the_post();
 
-<?php endwhile; ?>
+get_template_part('template-parts/content-event');
+  
+endwhile; ?>
 <?php print paginate_links(); ?>
 
 <hr class="section-break">
